bfe3fdc7f121b5cf48feb03bab3d3989b5f01dca
alilinykh <a.f.ilinykh@gmail.com>
2019-12-20 19:20:16 -0500

    add bookitem.vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <li>{{book.title}}:{{book.author}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"],
+  data() {}
+};
+</script>
+
+<style scoped>
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+</style>
